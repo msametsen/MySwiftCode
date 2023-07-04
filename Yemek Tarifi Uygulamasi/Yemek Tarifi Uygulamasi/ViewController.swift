4e891ddf6b0f6ee8d8de892792891591fce932a4
SAMET ŞEN <96656977+msametsen@users.noreply.github.com>
2023-07-04 11:49:34 +0300

    Add files via upload

---
+++ Yemek Tarifi Uygulamasi/Yemek Tarifi Uygulamasi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Yemek Tarifi Uygulamasi
+//
+//  Created by Samet on 1.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
