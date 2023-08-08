29d595678f26a90f18252e6c1c76b8f810efbe51
SAMET ŞEN <96656977+msametsen@users.noreply.github.com>
2023-08-08 18:38:10 +0300

    Add files via upload

---
+++ Basit Collection View/Basit Collection View/CollectionViewHucre.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewHucre.swift
+//  Basit Collection View
+//
+//  Created by Samet on 8.08.2023.
+//
+
+import UIKit
+
+class CollectionViewHucre: UICollectionViewCell {
+    
+    @IBOutlet weak var hucreLabel: UILabel!
+    
+}
